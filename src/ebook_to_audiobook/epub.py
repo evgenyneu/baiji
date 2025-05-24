858d58495018013735fb7f05d85aea3d599c42a7
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2025-05-24 12:08:51 +0000

    epub

--- src/ebook_to_audiobook/epub.py
+++ src/ebook_to_audiobook/epub.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from ebooklib import epub
+from ebooklib import epub, ITEM_DOCUMENT
 from bs4 import BeautifulSoup
 
 
@@ -13,7 +13,7 @@ def epub_to_chapters(path: str) -> List[str]:
     chapters = []
 
     for item in book.get_items():
-        if item.get_type() == epub.ITEM_DOCUMENT:
+        if item.get_type() == ITEM_DOCUMENT:
             soup = BeautifulSoup(item.get_content(), 'html.parser')
             text = soup.get_text(separator='\n', strip=True)
 
